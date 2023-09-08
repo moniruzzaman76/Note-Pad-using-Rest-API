d04a295ae8f8134bc0d2d55eb8e7f6c301dc8d4a
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-09-08 11:56:44 +0600

    code refactor

--- lib/ui/assets_utils/assets_utils.dart
+++ lib/ui/assets_utils/assets_utils.dart
@@ -3,4 +3,6 @@ class AssetUtils{
   static const String background = "$_imagePath/background.svg";
   static const String splashLogo = "$_imagePath/splash_screen.png";
   static const String profilePic = "$_imagePath/profile_picture.jpg";
+  static const String notesLogo = "$_imagePath/notes_logo.png";
+  static const String notesIcon = "$_imagePath/notes_icon.png";
 }
--- lib/ui/screen/splash_screen.dart
+++ lib/ui/screen/splash_screen.dart
@@ -41,28 +41,42 @@ class _SplashScreenState extends State<SplashScreen> {
               child:  Center(
                 child: Column(
                   children: [
-                    const SizedBox(height: 100,),
+                    const SizedBox(height: 140,),
                     const Text("Welcome To",style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 35,
-                        color: Colors.green
+                        color: Colors.teal
                     ),),
                     const SizedBox(height: 15,),
-                    const Icon(Icons.android_outlined,color: Colors.green,size: 70,),
-                    const SizedBox(height: 15,),
-                    const Text("Node Pad",style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 50,
-                      color: Colors.blue,
-                      letterSpacing: 4,
-                    ),),
-                    const SizedBox(height: 50,),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                         Icon(Icons.android_outlined,color: Colors.green,size: 50,),
+                         SizedBox(width:8,),
+                         Icon(Icons.apple_outlined,color: Colors.grey,size: 50,),
+                        // SizedBox(width:8,),
+                        // Icon(Icons.web_outlined,color: Colors.blue,size: 50,),
+                      ],
+                    ),
+                    // const SizedBox(height: 15,),
+                    // const Text("Node Pad",style: TextStyle(
+                    //   fontWeight: FontWeight.bold,
+                    //   fontSize: 50,
+                    //   color: Colors.blue,
+                    //   letterSpacing: 4,
+                    // ),),
+                    const SizedBox(height: 35,),
                     Image.asset(
-                      AssetUtils.splashLogo,
-                      height: 300,
+                      // AssetUtils.splashLogo,
+                      // AssetUtils.notesLogo,
+                      AssetUtils.notesIcon,
+                      height: 220,
                       width: double.infinity,
                       fit: BoxFit.cover,
                     ),
+                    const SizedBox(height: 220,),
+                    const Text("Version 1.2.0",style: TextStyle(color: Colors.grey,fontSize: 18),)
                   ],
                 ),
               )
